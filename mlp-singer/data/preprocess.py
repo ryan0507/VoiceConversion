8199d1b1eb4ea5e5aea170120cdcb8518d96f7c7
SunJong Park <ryan0507@yonsei.ac.kr>
2021-12-13 02:30:55 +0900

    MLP-singer inference finish

--- mlp-singer/data/preprocess.py
+++ mlp-singer/data/preprocess.py
@@ -74,6 +74,8 @@ def align(
     min_note: int,
 ):
     """align phonemes with notes and expand to mel-spectrogram length"""
+    print(len(note_sequence))
+    print(len(phonemes))
     assert len(phonemes) == len(note_sequence), print("midi lyrics mismatch")
     expanded_text = torch.zeros(total_length)
     expanded_notes = torch.zeros(total_length)
